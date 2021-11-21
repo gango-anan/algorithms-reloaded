333791d76c77563c0dc188afe4c07083ba456ee7
Galiwango Ananiya <galiwango101@gmail.com>
2021-11-21 23:02:14 +0300

    Add shiftItems utility method for shifting items after deleting a value at a specific index.

--- JavaScript/Arrays/customeArray.js
+++ JavaScript/Arrays/customeArray.js
@@ -20,4 +20,18 @@ class MyArray {
     this.length -= 1;
     return lastItem;
   }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length - 1];
+    this.length -= 1;
+  }
+  
 }
