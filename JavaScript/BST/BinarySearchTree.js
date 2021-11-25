864d4d5dcf7cc477d627ff6542ae98be3bb4a9f9
Galiwango Ananiya <galiwango101@gmail.com>
2021-11-25 13:24:43 +0300

    Build a binary search tree.

---
+++ JavaScript/BST/BinarySearchTree.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+
+  insert() {
+
+  }
+
+  lookUp() {
+
+  }
+}
+
+const tree = new BinarySearchTree();
