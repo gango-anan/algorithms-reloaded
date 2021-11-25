7e17a00da6b108f61d268f85d94752e476cef3de
Galiwango Ananiya <galiwango101@gmail.com>
2021-11-25 14:50:42 +0300

    Implement BST insert method.

--- JavaScript/BST/BinarySearchTree.js
+++ JavaScript/BST/BinarySearchTree.js
@@ -11,11 +11,31 @@ class BinarySearchTree {
     this.root = null
   }
 
-  insert() {
-
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right
+        }
+      }
+    }
   }
 
-  lookUp() {
+  lookUp(value) {
 
   }
 }
