24dadb5f0631263eebca43c8521f38035f5e1d7f
Galiwango Ananiya <galiwango101@gmail.com>
2022-11-16 02:20:59 +0300

    fix the JavaScript power_set algorithm where the method was ending up in an infinite loop

--- Algo/JavaScript/Recursion/powerset.js
+++ Algo/JavaScript/Recursion/powerset.js
@@ -1,10 +1,15 @@
 function powerset(array) {
-  subSets = [[]];
-  array.forEach((elem) => {
-    for (let index = 0; index < subSets.length; index++) {
-      currentSubset = subSets[index];
-      subSets.push(currentSubset.concat([elem]));
-    }
-  });
-  return subSets;
+	const subSets = [[]];
+	array.forEach((elem) => {
+		const subSetsLength = subSets.length;
+		for (let index = 0; index < subSetsLength; index++) {
+			const currentSubset = subSets[index];
+			subSets.push(currentSubset.concat([elem]));
+		}
+	});
+	return subSets;
 }
+
+array = [1, 2, 3];
+
+console.log(powerset(array));
